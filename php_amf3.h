94061f84d4bb420c4ef0766bbec29ad96e611abe
neoxic <arseny.vakhrushev@me.com>
2021-12-26 21:15:25 -0800

    Bump version to 2.1.0

--- php_amf3.h
+++ php_amf3.h
@@ -24,7 +24,7 @@
 #define PHP_AMF3_H
 
 
-#define PHP_AMF3_VERSION "2.0.3"
+#define PHP_AMF3_VERSION "2.1.0"
 
 extern zend_module_entry amf3_module_entry;
 #define phpext_amf3_ptr &amf3_module_entry
